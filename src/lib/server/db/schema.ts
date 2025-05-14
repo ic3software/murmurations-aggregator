d9027f3aa4bdd3480097b80444ea2a8405774b7c
vthwang <vincent@fishboneapps.com>
2025-05-13 21:00:12 -0700

    feat: setup drizzle and schema
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,17 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
-export const user = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	age: integer('age')
+import { sqliteTable, integer, text, real } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+
+export const clusters = sqliteTable('clusters', {
+	id: integer('id').primaryKey().notNull(),
+	name: text('name').notNull(),
+	indexUrl: text('index_url').notNull(),
+	queryUrl: text('query_url').notNull(),
+	tagSlug: text('tag_slug').notNull().unique(),
+	mapCenterLat: real('map_center_lat').default(46.603354).notNull(),
+	mapCenterLon: real('map_center_lon').default(1.888334).notNull(),
+	mapScale: integer('map_scale').default(5).notNull(),
+	lastUpdated: integer('last_updated', { mode: 'timestamp' }).notNull().default(sql`CURRENT_TIMESTAMP`),
+	createdAt: integer('created_at').default(sql`CURRENT_TIMESTAMP`).notNull(),
+	updatedAt: integer('updated_at').default(sql`CURRENT_TIMESTAMP`).notNull()
 });
