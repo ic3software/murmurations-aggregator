4759634fa47e6fa7a8f3db0429ce7160b0c0d33b
vthwang <vincent@fishboneapps.com>
2025-07-07 21:16:29 -0700

    fix: fix error message
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/routes/profile-generator/+page.ts
+++ src/routes/profile-generator/+page.ts
@@ -4,14 +4,19 @@ import type { BasicSchema } from '$lib/types/schema';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-	const { data: schemas, error } = await getLibrarySchemas(fetch);
-	const schemasList = schemas
-		.filter((s: BasicSchema) => {
-			return !s.name.startsWith('default-v');
-		})
-		.filter((s: BasicSchema) => {
-			return !s.name.startsWith('test_schema-v');
-		});
+	try {
+		const { data: schemas, error } = await getLibrarySchemas(fetch);
+		const schemasList = schemas
+			.filter((s: BasicSchema) => {
+				return !s.name.startsWith('default-v');
+			})
+			.filter((s: BasicSchema) => {
+				return !s.name.startsWith('test_schema-v');
+			});
 
-	return { schemasList, errorMessage: error };
+		return { schemasList, errorMessage: error };
+	} catch (err) {
+		console.error('Error fetching schemas:', err);
+		return { schemasList: [], errorMessage: 'Error fetching schemas' };
+	}
 };
