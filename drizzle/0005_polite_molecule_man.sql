aacb108857083ca9a20e22463201c440d449e1cc
vthwang <vincent@fishboneapps.com>
2025-09-17 20:30:27 -0700

    refactor: clean up the migrations
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- drizzle/0005_polite_molecule_man.sql
+++ drizzle/0005_polite_molecule_man.sql
@@ -123,3 +123,9 @@ SELECT
   id,
   1
 FROM users;
+--> statement-breakpoint
+-- Add the users capability to the User role
+INSERT INTO `role_capabilities` (`role_id`, `capability_id`)
+SELECT 
+  (SELECT id FROM roles WHERE name = 'User'),
+  (SELECT id FROM capabilities WHERE scheme = 'api' AND hier_part = '/users' AND namespace = 'users' AND segments = 'GET');
--- drizzle/0007_add_user_capability.sql
+++
@@ -1,5 +0,0 @@
--- Add the users capability to the User role
-INSERT INTO `role_capabilities` (`role_id`, `capability_id`)
-SELECT 
-  (SELECT id FROM roles WHERE name = 'User'),
-  (SELECT id FROM capabilities WHERE scheme = 'api' AND hier_part = '/users' AND namespace = 'users' AND segments = 'GET');
