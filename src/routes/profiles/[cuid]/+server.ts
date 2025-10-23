e6597c29a02b4500972220b165ca537be0141a64
vthwang <vincent@fishboneapps.com>
2025-10-23 15:43:25 -0700

    fix: fix cors location
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/routes/profiles/[cuid]/+server.ts
+++ src/routes/profiles/[cuid]/+server.ts
@@ -40,3 +40,14 @@ export const GET: RequestHandler = async ({
 		);
 	}
 };
+
+export const OPTIONS: RequestHandler = async () => {
+	return new Response(null, {
+		status: 204,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+		}
+	});
+};
